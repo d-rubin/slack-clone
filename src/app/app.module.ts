c6f74b0ad4075f090f7de518758559979c2db119
Daniel Rubin <daniel.rubin1997@gmail.com>
2022-12-21 17:05:25 +0100

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { MainareaComponent } from './mainarea/mainarea.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    LoginComponent,
+    SignUpComponent,
+    MainareaComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
