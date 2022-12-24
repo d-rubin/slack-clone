30ed6ccc759ae76e46f63ab44df0c86eb3e8cc73
Daniel Rubin <daniel.rubin1997@gmail.com>
2022-12-24 19:26:35 +0100

    Add Channel to database

--- src/app/channel-dialog/channel-dialog.component.ts
+++ src/app/channel-dialog/channel-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from "@angular/material/dialog";
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-channel-dialog',
@@ -8,9 +9,22 @@ import { MatDialogRef } from "@angular/material/dialog";
 })
 export class ChannelDialogComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<ChannelDialogComponent>) { }
+  constructor(
+    public dialogRef: MatDialogRef<ChannelDialogComponent>, 
+    public firestore: AngularFirestore
+    ) { }
+
+  channelName: string;
 
   ngOnInit(): void {
   }
 
+  createChannel() {
+    this.firestore
+    .collection('channels')
+    .add(this.channelName)
+    .then(() => {
+      this.dialogRef.close();
+    });
+  }
 }
