0a4b52f3b9796580a099d2290c38b0460b5e633f
Daniel Rubin <daniel.rubin1997@gmail.com>
2022-12-27 22:22:32 +0100

    little fix

--- src/app/models/Conversation.class.ts
+++ src/app/models/Conversation.class.ts
@@ -1,6 +1,6 @@
 export class Conversation {
     members: string[];
-    messages: JSON; // Should get a Format like { message: [], userId: [] } --> Message[1] and userId[1] is connected
+    messages: JSON; // Should get a Format like { message: [], userEmail: [] } --> Message[1] and userEmail[1] is connected
 
     constructor(obj?: any) {
         this.members = obj? obj.members: [];
