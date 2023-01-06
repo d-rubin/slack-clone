b7823ef5559868054ef99be26d4e7897fb7f0434
Daniel Rubin <daniel.rubin1997@gmail.com>
2023-01-06 21:00:44 +0100

    Try to implement channel name

--- src/app/chatbox-menu/chatbox-menu.component.ts
+++ src/app/chatbox-menu/chatbox-menu.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddMemberComponent } from '../add-member/add-member.component';
+import { User } from '../models/user.class';
+import { DataService } from '../services/data.service';
 import { ShowMembersComponent } from '../show-members/show-members.component';
 
 @Component({
@@ -9,12 +11,25 @@ import { ShowMembersComponent } from '../show-members/show-members.component';
   styleUrls: ['./chatbox-menu.component.scss']
 })
 export class ChatboxMenuComponent implements OnInit {
-  name: string = 'NAME';
+  name: string;
   members: number = 15;
+  currentUserIdFirestore: any;
+  currentUserEmail: any;
+  currentUser: any;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(
+    public dialog: MatDialog,
+    public dataService: DataService
+    ) {
 
-  ngOnInit(): void {
+    }
+
+  async ngOnInit() {
+    this.currentUserEmail = await this.dataService.onAuthStateChanged();
+    this.currentUserIdFirestore = await this.dataService.getCurrentUserID();
+    this.currentUser = await this.dataService.getCurrentUserData();
+    this.currentUser.currentUserId = this.currentUserIdFirestore;
+    console.log(this.currentUser)
   }
 
   showMembers() {
